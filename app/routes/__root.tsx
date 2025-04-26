68fed7d6f6eaf645f799bc06af1c3ba3515f2116
ulysses316 <ulises316@live.com.mx>
2025-04-26 01:51:35 -0600

    [ADD] Biome rules

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,48 +1,48 @@
-// app/routes/__root.tsx
-import type { ReactNode } from 'react'
 import {
-  Outlet,
-  createRootRoute,
-  HeadContent,
-  Scripts,
-} from '@tanstack/react-router'
+	HeadContent,
+	Outlet,
+	Scripts,
+	createRootRoute,
+} from "@tanstack/react-router";
+// app/routes/__root.tsx
+import type { ReactNode } from "react";
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-  }),
-  component: RootComponent,
-})
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "TanStack Start Starter",
+			},
+		],
+	}),
+	component: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="es">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	);
 }
--- app/ssr.tsx
+++ app/ssr.tsx
@@ -1,13 +1,13 @@
+import { getRouterManifest } from "@tanstack/react-start/router-manifest";
 // app/ssr.tsx
 import {
-    createStartHandler,
-    defaultStreamHandler,
-  } from '@tanstack/react-start/server'
-  import { getRouterManifest } from '@tanstack/react-start/router-manifest'
-  
-  import { createRouter } from './router'
-  
-  export default createStartHandler({
-    createRouter,
-    getRouterManifest,
-  })(defaultStreamHandler)
+	createStartHandler,
+	defaultStreamHandler,
+} from "@tanstack/react-start/server";
+
+import { createRouter } from "./router";
+
+export default createStartHandler({
+	createRouter,
+	getRouterManifest,
+})(defaultStreamHandler);
